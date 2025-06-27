dc083552c3a5c050926fcac0d0cad51dedcfe55f
abhi2J4 <[200~148936912+abhi2J4@users.noreply.github.com ~>
2025-06-27 09:41:09 +0530

    useEffect used

---
+++ src/components/Shimmer.js
@@ -0,0 +1,28 @@
+const Shimmer = () =>{
+    return (
+      <div className="shimmer_container">
+        <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
+         <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
+         <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
+         <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
+         <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card4"></div>
+      </div>
+
+    )
+}
+export default Shimmer;
